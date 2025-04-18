d72bf0774112a0d4428d29e8d2848c98dc6a61be
Paulo Samuel <paulosamuelr2024@icloud.com>
2025-04-17 21:34:51 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem? ')
+    c++ // c = c + 1
+}
+*/
+
+/*
+var c = 1
+do {
+    console.log('Tudo bem? ')
+    c++
+} while (c <= 6)
+*/
+
+var n1 = 2
+var c = 1
+while (c <= 10) {
+    var s = n1 * c
+    console.log(`${n1} x ${c} = ${s}`)
+    c++
+}
