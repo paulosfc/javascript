c97bbdc0b2b126322e53d4c727889bf000c10868
Paulo Samuel <paulosamuelr2024@icloud.com>
2025-04-25 18:32:25 -0300

    ex016

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,11 +1,19 @@
-function gerar() {
-    var n1 = document.getElementById('num')
-    var res = document.querySelector("div#res")
+function tabuada() {
+    var num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
 
-    var c = 1
-    while (n1 <= 10) {
-        var s = n1 * c
-        res.innerHTML = `${n1} x ${c} = ${s}`
-        c++
+    if (num.value.length == 0) {
+        window.alert('Por favor, Digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
